6355ae2be997d9a7490ddf0b23946f918e0333ce
Sandra Santhosh <102308614+aquiladive@users.noreply.github.com>
2025-08-02 18:06:17 +0530

    Create MyCamelRoute.java

---
+++ BasicRoutes/MyCamelRoute.java
@@ -0,0 +1,12 @@
+import org.apache.camel.builder.RouteBuilder;
+public class MyCamelRoute extends RouteBuilder {
+
+  //change according to local settings
+  private String desktopPath = "/Users/username/Desktop/";
+
+    @Override public void configure() throws Exception
+    {
+        from("file:"+desktopPath+"Start?noop=true")
+                .to("file:"+desktopPath+"End");
+    }
+}
