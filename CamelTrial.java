4a0fb0e0f7e5cce9c0fe97bbd518abdb5968b9a9
Sandra Santhosh <102308614+aquiladive@users.noreply.github.com>
2025-08-02 18:02:37 +0530

    Create CamelTrial.java

---
+++ CamelTrial.java
@@ -0,0 +1,14 @@
+import org.apache.camel.CamelContext;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class CamelTrial {
+    public static void main(String[] args) throws Exception
+    {
+        CamelContext context = new DefaultCamelContext();
+        context.addRoutes(new MyCamelRoute());
+
+        context.start();
+        Thread.sleep(5000);
+        context.stop();
+    }
+}
