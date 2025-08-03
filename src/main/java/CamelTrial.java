c1b102dc927ed9da862aabd80f0ba2847a06ccbe
Sandra Santhosh <102308614+aquiladive@users.noreply.github.com>
2025-08-03 22:38:18 +0530

    Update CamelTrial.java

--- src/main/java/CamelTrial.java
+++ src/main/java/CamelTrial.java
@@ -6,7 +6,9 @@ public class CamelTrial {
     public static void main(String[] args) throws Exception
     {
         CamelContext context = new DefaultCamelContext();
-        context.addRoutes(new MyCamelRoute());
+        
+        //context.addRoutes(new MyCamelRoute());
+        context.addRoutes(new ExceptionHandlingRoute());
 
         context.start();
         Thread.sleep(5000);
