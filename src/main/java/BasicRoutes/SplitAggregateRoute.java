7a340b2d3d7e64b5cc85abf64d6788d4dac0e12c
Sandra Santhosh <102308614+aquiladive@users.noreply.github.com>
2025-08-04 19:44:02 +0530

    Create SplitAggregateRoute.java

---
+++ src/main/java/BasicRoutes/SplitAggregateRoute.java
@@ -0,0 +1,20 @@
+package BasicRoutes;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.processor.aggregate.StringAggregationStrategy;
+
+public class SplitAggregateRoute extends RouteBuilder {
+
+  //change according to local settings
+  private String desktopPath = "/Users/username/Desktop/";
+
+    @Override public void configure() throws Exception
+    {
+        from("file:"+desktopPath+"Start?noop=true")
+                .split().tokenize("\n") //splitting by newline
+                .aggregate(simple("${body} contains 'London'"), new StringAggregationStrategy())
+                .completionSize(4) //aggregate after 4 messages
+                .log(header("CamelFileName") + ": Aggregation complete.")
+                .to("file:"+desktopPath+"End");
+    }
+}
