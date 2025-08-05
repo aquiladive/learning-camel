39557995b6006f62c2576acfcdc91ba29b627b26
Sandra Santhosh <102308614+aquiladive@users.noreply.github.com>
2025-08-05 23:34:14 +0530

    Update CamelMailRoute.java

--- src/main/java/BasicRoutes/CamelMailRoute.java
+++ src/main/java/BasicRoutes/CamelMailRoute.java
@@ -1,11 +1,21 @@
-public class CamelMailRoute extends RouteBuilder {
-  onException(Exception.class)
-    .wiretap()
-    .to("file:"+desktopPath+"Error");
+package BasicRoutes;
 
-  from("file:"+desktopPath+"Start?noop=true")
-    .unmarshal().json()
-    .marshal().json()
-    .log("JSON file detected.")
-    .to("file:"+desktopPath+"End");
+import org.apache.camel.builder.RouteBuilder;
+
+public class CamelMailRoute extends RouteBuilder {
+  String desktopPath = "/Users/username/Desktop";
+  String smtpPath = "smtp://localhost:3025";
+  String smtpParams = "username=test@localhost&password=password";
+  
+  @Override public void configure() throws Exception {
+    onException(Exception.class)
+      .wiretap(smtpPath+"?"+smtpParams)
+      .to("file:"+desktopPath+"Error");
+
+    from("file:"+desktopPath+"Start?noop=true")
+      .unmarshal().json()
+      .marshal().json()
+      .log("JSON file detected.")
+      .to("file:"+desktopPath+"End");
+  }
 }
