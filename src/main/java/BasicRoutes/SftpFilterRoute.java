f33054e0a46507f9f2c7b0e9b5de8edb4d2cb49a
Sandra Santhosh <102308614+aquiladive@users.noreply.github.com>
2025-08-28 23:57:11 +0530

    Create SftpFilterRoute.java

---
+++ src/main/java/BasicRoutes/SftpFilterRoute.java
@@ -0,0 +1,22 @@
+package BasicRoutes;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class CamelMailRoute extends RouteBuilder {
+  String desktopPath = "/Users/username/Desktop";
+  String sftpPath = "sftp://localhost:22";
+  String privateKey = "src/main/resources/privateKey";
+  String sftpParams = "username=user&privateKey="+privateKey;
+
+  String filterPath = URLEncoder.encode("&filterFile={file:size}<500");
+  
+  @Override
+  public void configure() throws Exception {
+    from("file:"+desktopPath+"Start?noop=true")
+      .log("JSON file detected.")
+      .to(sftpPath+"?"+sftpParams);
+
+    from(sftpPath+"?"+sftpParams+filterPath)
+      .to("file:"+desktopPath+"End");
+  }
+}
